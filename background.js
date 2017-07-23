03a087de4ef8c55920886a42c1fdd4827b951c65
Zeno Zeng <zenoofzeng@gmail.com>
2017-07-23 14:21:59 +0800

    normalize url

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
+function normalizeUrl(url) {
+    let end = "slack.com/messages";
+    if (url.indexOf(end) > -1) {
+        return url.substring(0, url.indexOf(end) + end.length);
+    }
+    return url;
+}
+
 function removeDuplicateTab(newTab) {
     chrome.tabs.getAllInWindow(newTab.windowId, function(tabs) {
         var duplicateTab = null;
         tabs.forEach(function(otherTab) {
-            if (otherTab.id !== newTab.id && otherTab.url === newTab.url) {
-                duplicateTab = otherTab;
+            if (otherTab.id !== newTab.id) {
+                if (normalizeUrl(otherTab.url) == normalizeUrl(newTab.url)) {
+                    duplicateTab = otherTab;
+                }
             }
         });
         if (duplicateTab) {
